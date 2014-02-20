28904182f2b4f58e00c05e383c478d4c17802ceb
Justin Santa Barbara <justin@fathomdb.com>
2014-02-19 16:09:21 -0800

    Add kinesis binding & tests

--- project/Build.scala
+++ project/Build.scala
@@ -22,13 +22,19 @@ object Build extends Build {
     settings = buildSettings ++ Seq(libraryDependencies ++= deps)
   )
 
-  val spray_dyanmodb = Project(
+  val spray_dynamodb = Project(
     id = "spray-dynamodb",
     base = file("spray-dynamodb"),
     settings = buildSettings ++ Seq(libraryDependencies ++= deps)
   ).dependsOn(spray_aws)
 
-  val root = Project(id = "spray-aws-project", base = file("."), settings = buildSettings ++ parentSettings).aggregate(spray_aws,spray_dyanmodb)
+  val spray_kinesis = Project(
+    id = "spray-kinesis",
+    base = file("spray-kinesis"),
+    settings = buildSettings ++ Seq(libraryDependencies ++= deps)
+  ).dependsOn(spray_aws)
+
+  val root = Project(id = "spray-aws-project", base = file("."), settings = buildSettings ++ parentSettings).aggregate(spray_aws,spray_dynamodb,spray_kinesis)
 
 
   def publishSettings: Seq[Setting[_]] = Seq(
---
+++ spray-kinesis/src/main/scala/com/sclasen/spray/aws/kinesis/KinesisClient.scala
@@ -0,0 +1,124 @@
+package com.sclasen.spray.aws.kinesis
+
+import akka.actor.{ ActorRefFactory, ActorSystem }
+import collection.JavaConverters._
+import com.amazonaws.services.kinesis.model._
+import com.amazonaws.services.kinesis.model.transform._
+import com.amazonaws.transform.JsonErrorUnmarshaller
+import concurrent.Future
+import java.util.{ List => JList }
+import akka.util.Timeout
+import com.sclasen.spray.aws._
+import com.amazonaws.services.kinesis.model.ListStreamsRequest
+import com.amazonaws.services.kinesis.model.ListStreamsResult
+
+case class KinesisClientProps(key: String, secret: String, operationTimeout: Timeout, system: ActorSystem, factory: ActorRefFactory, endpoint: String = "kinesis.us-east-1.amazonaws.com") extends SprayAWSClientProps {
+  val service = "kinesis"
+}
+
+object MarshallersAndUnmarshallers {
+  implicit val createStreamRequest = new CreateStreamRequestMarshaller()
+
+  implicit val deleteStreamRequest = new DeleteStreamRequestMarshaller()
+
+  implicit val describeStreamRequest = new DescribeStreamRequestMarshaller()
+  implicit val describeStreamResult = DescribeStreamResultJsonUnmarshaller.getInstance()
+
+  implicit val getRecordsRequest = new GetRecordsRequestMarshaller()
+  implicit val getRecordsResult = GetRecordsResultJsonUnmarshaller.getInstance()
+
+  implicit val getShardIteratorRequest = new GetShardIteratorRequestMarshaller()
+  implicit val getShardIteratorResult = GetShardIteratorResultJsonUnmarshaller.getInstance()
+
+  implicit val listStreamsRequest = new ListStreamsRequestMarshaller()
+  implicit val listStreamsResult = ListStreamsResultJsonUnmarshaller.getInstance()
+
+  implicit val mergeShardsRequest = new MergeShardsRequestMarshaller()
+
+  implicit val putRecordsRequest = new PutRecordRequestMarshaller()
+  implicit val putRecordsResult = PutRecordResultJsonUnmarshaller.getInstance()
+
+  implicit val splitShardRequest = new SplitShardRequestMarshaller()
+
+  val kinesisExceptionUnmarshallers: JList[JsonErrorUnmarshaller] = List(
+    new InvalidArgumentExceptionUnmarshaller(),
+    new LimitExceededExceptionUnmarshaller(),
+    new ResourceInUseExceptionUnmarshaller(),
+    new ResourceNotFoundExceptionUnmarshaller(),
+    new ExpiredIteratorExceptionUnmarshaller(),
+    new ProvisionedThroughputExceededExceptionUnmarshaller(),
+    new JsonErrorUnmarshaller()).toBuffer.asJava
+
+}
+
+class KinesisClient(val props: KinesisClientProps) extends SprayAWSClient(props) {
+
+  import MarshallersAndUnmarshallers._
+
+  val log = props.system.log
+
+  def exceptionUnmarshallers: JList[JsonErrorUnmarshaller] = kinesisExceptionUnmarshallers
+
+  /**
+   * Creates a stream.
+   *
+   * Returns 200 with an empty body on success.
+   */
+  def sendCreateStream(aws: CreateStreamRequest): Future[Void] =
+    pipeline(request(aws)).map({ response[Void](_)(null) })
+
+  /**
+   * Deletes a stream.
+   *
+   * Returns 200 with an empty body on success.
+   */
+  def deleteCreateStream(aws: DeleteStreamRequest): Future[Void] =
+    pipeline(request(aws)).map({ response[Void](_)(null) })
+
+  /**
+   * Get metadata about a stream.
+   */
+  def describeStream(aws: DescribeStreamRequest): Future[DescribeStreamResult] =
+    pipeline(request(aws)).map(response[DescribeStreamResult])
+
+  /**
+   * Get records from a specific shard.  Open the iterator first with getShardIterator.
+   */
+  def getRecords(aws: GetRecordsRequest): Future[GetRecordsResult] =
+    pipeline(request(aws)).map(response[GetRecordsResult])
+
+  /**
+   * Opens an iterator onto a specific shard.
+   */
+  def getShardIterator(aws: GetShardIteratorRequest): Future[GetShardIteratorResult] =
+    pipeline(request(aws)).map(response[GetShardIteratorResult])
+
+  /**
+   * Get list of all streams.
+   */
+  def listStreams(aws: ListStreamsRequest): Future[ListStreamsResult] =
+    pipeline(request(aws)).map(response[ListStreamsResult])
+
+  /**
+   * Merge two shards.
+   *
+   * Returns 200 with empty body on success.
+   */
+  def mergeShards(aws: MergeShardsRequest): Future[Void] =
+    pipeline(request(aws)).map({ response[Void](_)(null) })
+
+  /**
+   * Add a record to a stream.
+   */
+  def putRecord(aws: PutRecordRequest): Future[PutRecordResult] =
+    pipeline(request(aws)).map(response[PutRecordResult])
+
+  /**
+   * Split a shard into two.
+   *
+   * Returns 200 with empty body on success.
+   */
+  def splitShard(aws: SplitShardRequest): Future[Void] =
+    pipeline(request(aws)).map({ response[Void](_)(null) })
+
+}
