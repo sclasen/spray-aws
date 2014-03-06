aed0fee323e4a6142207cc901ed1df0891bf6db3
Scott Clasen <scott@heroku.com>
2014-03-06 11:03:22 -0800

    next-snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object Build extends Build {
 
   val buildSettings = Seq(
     organization := "com.sclasen",
-    version := "0.2.4",
+    version := "0.2.5-SNAPSHOT",
     scalaVersion := "2.10.2",
     crossScalaVersions := Seq("2.10.2"),
     resolvers ++= Seq("TypesafeMaven" at "http://repo.typesafe.com/typesafe/maven-releases",
