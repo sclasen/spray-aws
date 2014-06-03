f5cdff281ce210cc06bb5ed53ba293631b58a2b3
Scott Clasen <scott@heroku.com>
2014-06-03 12:58:25 -0700

    bump to 0.3.1-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object Build extends Build {
 
   val buildSettings = Seq(
     organization := "com.sclasen",
-    version := "0.3.0",
+    version := "0.3.1-SNAPSHOT",
     scalaVersion := "2.11.1",
     crossScalaVersions := Seq("2.10.4", "2.11.1"),
     scalacOptions ++= Seq("-feature", "-deprecation", "-language:implicitConversions", "-language:postfixOps"),
